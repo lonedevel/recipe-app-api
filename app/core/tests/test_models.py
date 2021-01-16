24ae014ee43f566c3debf42f250d031402eba30b
rmichaud <rmichaud@icloud.com>
2021-01-15 21:31:25 -0500

    Added custom user model and test cases.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@emaildomain.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test if the email for a new user is normalized"""
+        email = 'test@EMAILDOMAIN.COM'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating a user without an email address raises error"""
+        email = None
+        password = 'Testpass123'
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=email,
+                password=password
+            )
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        email = 'test@emaildomain.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
